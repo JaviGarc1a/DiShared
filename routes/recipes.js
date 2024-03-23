242fffe3d784889f17b0564b07eb8ab6adb03f77
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-23 17:26:52 +0100

    feat: Recipes GET

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,32 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Recipes
+ *   description: Recipe management and retrieval
+ * /recipes:
+ *   get:
+ *     summary: Returns the list of all the recipes
+ *     tags: [Recipes]
+ *     responses:
+ *       200:
+ *         description: The list of the Recipes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Recipe'
+ *             type: array
+ */
+
 var express = require('express')
 var router = express.Router()
-
 const authMiddleware = require('../middlewares/authMiddleware')
+
+const Recipe = require('../models/recipe')
+
+// GET all recipes
+router.get('/', async function (req, res, next) {
+  const recipes = await Recipe.find()
+  res.json(recipes)
+})
+
 module.exports = router
