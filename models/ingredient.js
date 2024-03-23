a179a6215a2c8580b77c4e0e7a998657a6361a0d
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-23 12:59:13 +0100

    fix: Closing brackets

--- models/ingredient.js
+++ models/ingredient.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const IngredientSchema = new Schema({
-    name: { type: String, unique: true, required: true },
-},
+  name: { type: String, unique: true },
 })
 
 IngredientSchema.virtual('url').get(function () {
-    return `/ingredients/${this._id}`
+  return `/ingredients/${this._id}`
 })
 
 const Ingredient = mongoose.model('Ingredient', IngredientSchema)
