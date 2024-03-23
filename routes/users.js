6e9b65eb82122d7d36439e84d37de962305627e5
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-23 13:12:18 +0100

    feat: Get users request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const User = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', async function (req, res, next) {
+  // Get users without the password field
+  const users = await User.find({}, '-password')
+  res.json(users)
+})
 
-module.exports = router;
+module.exports = router
