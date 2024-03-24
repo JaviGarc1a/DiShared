3a98dd428e47364a57962328de530f348ea63f36
JaviGarc1a <javgaragu1@alum.us.es>
2024-03-24 17:17:12 +0100

    docs: Add ingredient schema swagger

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,3 +1,22 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *           Ingredient:
+ *               type: object
+ *               required:
+ *                   - name
+ *               properties:
+ *                   _id:
+ *                       type: string
+ *                       description: The auto-generated id of the ingredient
+ *                   name:
+ *                       type: string
+ *                       description: The name of the ingredient
+ *               example:
+ *                   _id: 660046e12b7446bea8c470bd
+ *                   name: Salt
+ */
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
