265cc62d8b27ede75f4493e40f2e7ff1802e190a
JaviGarc1a <javgaragu1@alum.us.es>
2024-03-24 17:21:04 +0100

    feat: Add update ingredient

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -243,4 +243,20 @@ router.get('/:id', authMiddleware, async function (req, res) {
   }
 })
 
+/* PUT update ingredient by ID */
+router.put('/:id', authMiddleware, async function (req, res) {
+  const { name } = req.body
+
+  try {
+    const updateIngredient = await Ingredient.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    )
+    res.status(200).json(updateIngredient)
+  } catch (error) {
+    return res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
 module.exports = router
