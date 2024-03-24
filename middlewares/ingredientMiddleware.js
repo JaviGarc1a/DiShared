e3f6b48080a8bf18c598ef0c98b8c866e06caad6
JaviGarc1a <javgaragu1@alum.us.es>
2024-03-24 17:37:09 +0100

    feat: Add middleware to check if ingredient not exist

--- middlewares/ingredientMiddleware.js
+++ middlewares/ingredientMiddleware.js
@@ -10,4 +10,17 @@ const ingredientExistMiddleware = async (req, res, next) => {
   next()
 }
 
-module.exports = { ingredientExistMiddleware }
+const ingredientNotExistMiddleware = async (req, res, next) => {
+  // Check if the ingredient does not exist
+  try {
+    const ingredient = await Ingredient.findById(req.params.id)
+    if (!ingredient) {
+      return res.status(404).json({ message: 'Ingredient not found.' })
+    }
+    next()
+  } catch (error) {
+    return res.status(404).json({ message: 'Ingredient not found.' })
+  }
+}
+
+module.exports = { ingredientExistMiddleware, ingredientNotExistMiddleware }
